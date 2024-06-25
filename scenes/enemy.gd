ff7074475b6a7a189d5a51d098191820cb149f28
Ryan Price <rsprice90@gmail.com>
2024-06-24 20:47:10 -0700

    feat: basic project setup

---
+++ scenes/enemy.gd
@@ -0,0 +1,84 @@
+extends CharacterBody2D
+
+enum ORIENTATION {LEFT, RIGHT, UP, DOWN}
+const STATE: Dictionary =  {
+	"ATTACK": "attack",
+	"WALK": "walk",
+	"DIE": "die"
+}
+
+const FACINGS: Dictionary = {
+	ORIENTATION.UP: "u",
+	ORIENTATION.DOWN: "d",
+	ORIENTATION.LEFT: "s",
+	ORIENTATION.RIGHT: "s",
+}
+
+@export var speed: float = 50.0
+@export var health: int = 100
+@export var current_state: Dictionary = {
+	"action": STATE.WALK,
+	"orientation": FACINGS[ORIENTATION.UP],
+}
+
+@onready var last_global_position: Vector2 = global_position
+@onready var animated_sprite = $AnimatedSprite2D
+@onready var aggro_radius = $AggroRadius
+@onready var hitbox = $CollisionShape2D
+
+func _ready():
+	pass
+
+
+func _physics_process(delta) -> void:
+	var target_position: Vector2 = get_target_position()
+	var direction: Vector2 = (target_position - global_position).normalized()
+	velocity = direction * speed
+	var orientation = get_orientation(direction)
+	var state = get_character_state()
+	animate(state, orientation)
+	var collision: KinematicCollision2D = move_and_collide(velocity * delta)
+	
+	if collision:
+		velocity = Vector2.ZERO
+
+	last_global_position = global_position
+
+
+func get_target_position():
+	if true:
+		return Vector2(get_viewport().size.x / 2, 0)
+	
+
+func move():
+	pass
+
+
+func get_orientation(direction: Vector2):
+	var orientation: String = ""
+
+	if abs(direction.x) > abs(direction.y):
+		orientation = "s"
+		if (direction.x > 0):
+			animated_sprite.flip_h = true
+		else:
+			animated_sprite.flip_h = false
+	else:
+		if direction.y < 0:
+			orientation = "u"
+		else:
+			orientation ="d"
+	
+	return orientation
+
+
+func animate(state: String, orientation: String) -> void:
+	var sprite_frames: SpriteFrames = animated_sprite.sprite_frames
+	var next_animation: String = state + "_" + orientation
+	assert(sprite_frames.has_animation(next_animation))
+	animated_sprite.play(next_animation)
+
+
+func get_character_state() -> String:
+	return "walk"
+
---
+++ scenes/main.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+@onready var main_menu_scene: CanvasLayer = $MainMenu
+var game_scene: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	main_menu_scene.connect("start_game", _on_start_game)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func _on_start_game():
+	print("starting game...")
+	game_scene = preload("res://scenes/game.tscn")
+	add_child(game_scene.instantiate())
+	main_menu_scene.queue_free()
