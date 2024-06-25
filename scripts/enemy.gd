4a489fbd5ca15f3ad8031e11e3f0b06b894d9815
Ryan Price <rsprice90@gmail.com>
2024-06-24 21:21:01 -0700

    feat: some docs in enemy.gd

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,11 +1,25 @@
 extends CharacterBody2D
+## Enemy AI script that deals with an enemy characters state and animations
+## In order for the script to work correctly it must be attached to the root
+## of a scene with the following Node tree
+##
+## CharacterBody2D (Ex: "Orc")
+## ├── AnimatedSprite2D
+## ├── Hitbox (CollisionShape2D)
+## └── AggroRadius (Area2D)
+##    └── CollisionShape2D
+## 
+## Assets should only have three directions: s, u, d, which correspond to
+## sideways, up, and down
+## 
+## Animation names should always be snake case ex: "walk_s", "attack_u"
 
 @export var speed: float = 50.0
 @export var health: int = 100
 @onready var last_global_position: Vector2 = global_position
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var aggro_radius = $AggroRadius
-@onready var hitbox = $CollisionShape2D
+@onready var hitbox = $Hitbox
 
 func _ready():
 	pass
@@ -15,11 +29,11 @@ func _physics_process(delta) -> void:
 	var target_position: Vector2 = get_target_position()
 	var direction: Vector2 = (target_position - global_position).normalized()
 	var velocity = direction * speed
-	var orientation = get_orientation(direction)
+	var orientation = get_orientation(direction) # NOTE: also flips the sprite!
 	var state = get_character_state()
 
 	animate(state, orientation)
-	
+
 	var collision: KinematicCollision2D = move_and_collide(velocity * delta)
 	
 	if collision:
@@ -31,10 +45,6 @@ func _physics_process(delta) -> void:
 func get_target_position():
 	if true:
 		return Vector2(get_viewport().size.x / 2, 0)
-	
-
-func move():
-	pass
 
 
 func get_orientation(direction: Vector2):
